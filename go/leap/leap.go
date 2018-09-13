781fd9cccf81a268ceab5e95d9114cf56f4c44f5
Carlos Alarcon <jchuerva@github.com>
2018-09-13 23:40:15 +0200

    first solution leap go

---
+++ go/leap/leap.go
@@ -0,0 +1,11 @@
+package leap
+
+// IsLeapYear return if it's a leap year
+func IsLeapYear(year int) bool {
+
+	if year%4 == 0 && (year%100 != 0 || year%400 == 0) {
+		return true
+	}
+
+	return false
+}

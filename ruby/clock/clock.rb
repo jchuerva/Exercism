6c7cca9e4c66117dfedcc9ade64ce84dfc7c7b10
Carlos Alarcon <jchuerva@github.com>
2018-09-18 00:04:39 +0200

    ruby clock v2

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,34 +1,34 @@
 class Clock
-  attr_reader :hours
-  attr_reader :minutes
+  attr_reader :total_minutes
 
-  ONE_DAY = 24 * 60
+  ONE_DAY_IN_MIN = 24 * 60
 
   def initialize(hour: 0, minute: 0)
-    total_minutes = (hour * 60) + minute
-    @hours = min(total_minutes) / 60
-    @minutes = min(total_minutes) % 60
+    @total_minutes = (hour * 60) + minute
   end
 
   def to_s
-    format('%02d:%02d', @hours, @minutes)
+    format('%02d:%02d', hours, minutes)
   end
 
   def +(other)
-    Clock.new(hour: @hours + other.hours, minute: @minutes + other.minutes)
+    Clock.new(hour: hours, minute: minutes + other.total_minutes)
   end
 
   def -(other)
-    Clock.new(hour: @hours - other.hours, minute: @minutes - other.minutes)
+    Clock.new(hour: hours, minute: minutes - other.total_minutes)
   end
 
   def ==(other)
-    @hours == other.hours && @minutes == other.minutes
+    to_s == other.to_s
   end
 
   private
+  def hours
+    total_minutes % ONE_DAY_IN_MIN / 60
+  end
 
-  def min(total_min)
-    total_min % ONE_DAY
+  def minutes
+    total_minutes % 60
   end
 end

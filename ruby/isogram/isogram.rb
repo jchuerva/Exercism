bd432b6dcb3f09ce12f4ac3e7d8923cc098165fa
Carlos Alarcon <jchuerva@github.com>
2018-10-24 11:14:51 +0200

    ruby isogram v3

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,7 +1,18 @@
 # Isogram module comment
 module Isogram
+  VERSION = 3
   def self.isogram?(input)
-    cad = input.downcase.gsub(/\W+/, '')
-    cad.size == cad.chars.uniq.size
+    # cad = input.downcase.gsub(/\W+/, '')
+    cad = input.downcase.delete("^a-z").chars
+    cad == cad.uniq
   end
 end
+
+# module Isogram
+#   VERSION = '1b'
+
+#   def self.isogram?(input)
+#     letters = 'abcdefghijklmnopqrstuvwxyz'
+#     letters.chars.none? { |l| input.downcase.count(l) >= 2 }
+#   end
+# end

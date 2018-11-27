9dda43d62c052666a8b91c7448fffd8fd7f85bad
Carlos Alarcon <jchuerva@github.com>
2018-11-27 19:05:59 +0100

    ruby tournament v4

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,5 +1,6 @@
 # Tournament class comment
 class Tournament
+  COLUMN_ATTRIBUTES = %w[name matches_played win draw lost points].freeze
 
   def self.tally(input)
     @teams = {}
@@ -7,45 +8,62 @@ class Tournament
     results(sort_results)
   end
 
-  def self.remove_new_line(sentence)
+  private_class_method def self.remove_new_line(sentence)
     sentence.gsub("\n",'')
   end
 
-  def self.teams(input)
+  private_class_method def self.teams(input)
     (t_names, result) = teams_and_result(input)
 
     create_team(t_names)
 
-    # add points
     points(t_names, result)
   end
 
-  def self.results(results)
-    output = "Team                           | MP |  W |  D |  L |  P\n"
-    output + results.map { |team| attributes(team) + "\n" }.join
+  private_class_method def self.results(results)
+    row_title + results.sum('') { |team| row(team) + "\n" }
   end
 
-  def self.attributes(team)
-    # calculate points just before print out the attributes
-    team.points = team.calculate_points
-    # print each attribute
-    team.instance_variables.map { |attr| get_attribute(team, attr) }.join(' | ')
+  private_class_method def self.row_title
+    COLUMN_ATTRIBUTES.map { |attr| row_title_attr(attr) }.join(' | ') + "\n"
   end
 
-  def self.get_attribute(team, attr)
-    # convert symbol to str
-    if attr.to_s.tr(":@",'') == 'name'
-      team.instance_variable_get(attr).ljust(30)
+  private_class_method def self.row_title_attr(attr)
+    if attr == 'name'
+      sprintf("%-30s", 'Team')
     else
-      team.instance_variable_get(attr).to_s.rjust(2)
+      sprintf("%2s", attr_title(attr))
     end
   end
 
-  def self.sort_results
+  private_class_method def self.attr_title(attr)
+    attr.split('_').sum('') { |i| i.upcase[0] }
+  end
+
+  private_class_method def self.row(team)
+    # each attribute
+    COLUMN_ATTRIBUTES.map { |attr| get_attribute(team, attr) }.join(' | ')
+  end
+
+  private_class_method def self.get_attribute(team, attr)
+    if attr == 'name'
+      # team.instance_variable_get(attr).ljust(30)
+      sprintf('%-30s', team.instance_variable_get(conv_attribute_to_sym(attr)))
+    else
+      # team.instance_variable_get(attr).to_s.rjust(2)
+      sprintf('%2s', team.instance_variable_get(conv_attribute_to_sym(attr)))
+    end
+  end
+
+  private_class_method def self.conv_attribute_to_sym(attr)
+    ('@' + attr).to_sym
+  end
+
+  private_class_method def self.sort_results
     @teams.values.sort_by { |i| [-i.calculate_points, i.name] }
   end
 
-  def self.points(t_names, result)
+  private_class_method def self.points(t_names, result)
     t_home = find_team(t_names[0])
     t_away = find_team(t_names[1])
     case result
@@ -63,37 +81,41 @@ class Tournament
     end
   end
 
-  def self.create_team(t_names)
+  private_class_method def self.create_team(t_names)
     t_names.each do |name|
       @teams[name] = Team.new(name) unless @teams.has_key?(name)
     end
   end
 
-  def self.find_team(t_name)
+  private_class_method def self.find_team(t_name)
     @teams[t_name]
   end
 
-  def self.teams_and_result(input)
+  private_class_method def self.teams_and_result(input)
     dev_input = input.split(';')
     teams = dev_input[0..1]
     result = dev_input[2]
     [teams, result]
   end
 
+  private
   # Team class comment
   class Team
     attr_accessor :name, :matches_played, :win, :draw, :lost, :points
+    
     def initialize(name)
       @name = name
       @matches_played = 0
       @win = 0
       @draw = 0
       @lost = 0
+      @points = 0
     end
 
     def add_win
       @matches_played += 1
       @win += 1
+      @points = calculate_points
     end
 
     def add_loss
@@ -104,6 +126,7 @@ class Tournament
     def add_draw
       @matches_played += 1
       @draw += 1
+      @points = calculate_points
     end
 
     def calculate_points
@@ -113,5 +136,5 @@ class Tournament
 end
 
 module Version
-  VERSION = 3
+  VERSION = 4
 end

367b1a47b24b95def5d5fbbc269018a6de4e25a7
Carlos Alarcon <jchuerva@github.com>
2019-05-17 11:31:40 +0200

    pyhton pangram v2

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -5,17 +5,11 @@ def is_pangram(sentence):
 
   # pdb.set_trace()
   
-  letters = string.ascii_lowercase
-  lower_sentence = sentence.lower()
-  result = True
-  for item in letters:
-    if item in lower_sentence:
-      continue
-    else:
-      result = False
-      break
-  return result
+  alfabet = set(string.ascii_lowercase)
+  lower_sentence = set(sentence.lower())
+
+  return lower_sentence >= alfabet
 
       
 def version():
-  VERSION = 1
+  VERSION = 2

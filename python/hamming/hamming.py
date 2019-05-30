52c6b58e9b97b30c0f0fd7bead3ef685bc70c9c4
Carlos Alarcon <jchuerva@github.com>
2019-05-30 17:38:08 +0200

    python hamming v1

---
+++ python/hamming/hamming.py
@@ -0,0 +1,9 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) == len(strand_b):
+        return sum(1 for a, b in zip(strand_a, strand_b) if a != b)
+    else:
+        raise ValueError("error")
+
+
+def version():
+    VERSION = 1

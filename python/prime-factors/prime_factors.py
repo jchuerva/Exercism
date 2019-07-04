ebc6e355b32f231202fc8fb5a8f6b3cc988b05dc
Carlos Alarcon <jchuerva@github.com>
2019-07-04 20:26:32 +0200

    python prime_factors v2

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,17 +1,21 @@
+import primefac
+
+
 def __version__():
-    VERSION = 1
+    VERSION = 2
 
 
 def factors(value):
     if value == 1:
         return []
     else:
-        n = 2
-        factors = []
-        while value != 1:
-            if value % n == 0:
-                factors.append(n)
-                value = value / n
-            else:
-                n += 1
-        return factors
+        # n = 2
+        # factors = []
+        # while value != 1:
+        #     if value % n == 0:
+        #         factors.append(n)
+        #         value = value / n
+        #     else:
+        #         n += 1
+        # return factors
+        return list(primefac.primefac(value))

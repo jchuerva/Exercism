c96d1e871ee15a0b983896592129b34f9c118840
Carlos Alarcon <jchuerva@github.com>
2019-10-29 23:13:44 +0100

    python sum-of v3

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,7 +1,15 @@
 
 def sum_of_multiples(limit, multiples):
-    return sum([x for x in range(limit) if any([x % m == 0 for m in multiples if m != 0])])
+    return sum([
+        x
+        for x in range(limit)
+        if any(
+            [x % m == 0
+             for m in multiples
+             if m != 0]
+        )
+    ])
 
 
 class Version():
-    VERSION = 2
+    VERSION = 3

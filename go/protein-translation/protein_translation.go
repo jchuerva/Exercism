5abc59c7357a7e6b0c2656b8f62200cea67d5bd1
jchuerva <jchuerva@github.com>
2021-04-15 12:58:10 +0200

    go protein-translation

---
+++ go/protein-translation/protein_translation.go
@@ -0,0 +1,52 @@
+package protein
+
+import "errors"
+
+var translations = map[string]string{
+	"AUG": "Methionine",
+	"UUU": "Phenylalanine",
+	"UUC": "Phenylalanine",
+	"UUA": "Leucine",
+	"UUG": "Leucine",
+	"UCU": "Serine",
+	"UCC": "Serine",
+	"UCA": "Serine",
+	"UCG": "Serine",
+	"UAU": "Tyrosine",
+	"UAC": "Tyrosine",
+	"UGU": "Cysteine",
+	"UGC": "Cysteine",
+	"UGG": "Tryptophan",
+	"UAA": "STOP",
+	"UAG": "STOP",
+	"UGA": "STOP",
+}
+
+var ErrInvalidBase = errors.New("ErrInvalidBase")
+var ErrStop = errors.New("ErrStop")
+
+func FromCodon(codon string) (string, error) {
+	value, ok := translations[codon]
+
+	if !ok {
+		return "", ErrInvalidBase
+	}
+	if value == "STOP" {
+		return "", ErrStop
+	}
+	return value, nil
+}
+
+func FromRNA(input string) ([]string, error) {
+	var result []string
+	for i := 0; i <= len(input)-3; i += 3 {
+		aminoacid, ok := FromCodon(input[i : i+3])
+		if ok == ErrInvalidBase {
+			return result, ErrInvalidBase
+		} else if ok == ErrStop {
+			return result, nil
+		}
+		result = append(result, aminoacid)
+	}
+	return result, nil
+}

d91078ebce8122306ee6f01c20d6987befe8e27e
Carlos Alarcon <jchuerva@github.com>
2021-09-06 10:29:45 +0200

    go difference-of-squares v2

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,23 +1,28 @@
 package diffsquares
 
 func Version() string {
-	return "1"
+	return "2"
 }
 
 func SquareOfSum(num int) int {
-	sum := 0
-	for i := 1; i <= num; i++ {
-		sum += i
-	}
+	sum := num * (num + 1) / 2 // http://www.trans4mind.com/personal_development/mathematics/series/sumNaturalNumbers.htm#mozTocId914933
 	return sum * sum
+
+	// sum := 0
+	// for i := 1; i <= num; i++ {
+	// 	sum += i
+	// }
+	// return sum * sum
 }
 
 func SumOfSquares(num int) int {
-	sum := 0
-	for i := 1; i <= num; i++ {
-		sum += i * i
-	}
-	return sum
+	return num * (num + 1) * (2*num + 1) / 6 // http://www.trans4mind.com/personal_development/mathematics/series/sumNaturalSquares.htm#summation
+
+	// sum := 0
+	// for i := 1; i <= num; i++ {
+	// 	sum += i * i
+	// }
+	// return sum
 }
 
 func Difference(num int) int {

189e2808a9e2367524a2c94e39c16f0a3f2e754e
Carlos Alarcon <jchuerva@github.com>
2021-09-06 16:40:19 +0200

    go annalyns v1

---
+++ go/annalyns-infiltration/annalyns_infiltration.go
@@ -0,0 +1,30 @@
+package annalyn
+
+func Version() string {
+	return "1"
+}
+
+// CanFastAttack can be executed only when the knight is sleeping
+func CanFastAttack(knightIsAwake bool) bool {
+	return !knightIsAwake
+	// panic("Please implement the CanFastAttack() function")
+}
+
+// CanSpy can be executed if at least one of the characters is awake
+func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
+	return knightIsAwake || archerIsAwake || prisonerIsAwake
+	// panic("Please implement the CanSpy() function")
+}
+
+// CanSignalPrisoner can be executed if prisoner is awake and the archer is sleeping
+func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
+	return prisonerIsAwake && !archerIsAwake
+	// panic("Please implement the CanSignalPrisoner() function")
+}
+
+// CanFreePrisoner can be executed if prisoner is awake and the other 3 characters are asleep
+// or if Annalyn's pet dog is with her and the archer is sleeping
+func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
+	return (prisonerIsAwake && !knightIsAwake && !archerIsAwake) || (petDogIsPresent && !archerIsAwake)
+	// panic("Please implement the CanFreePrisoner() function")
+}
